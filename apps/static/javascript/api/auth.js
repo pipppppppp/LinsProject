d239497f082df85e7085c8d72223770fb036c3ab
pipppppppp <helinaputri9@gmail.com>
2026-07-10 04:25:55 +0700

    10.7.26 | pull completed

--- apps/static/javascript/api/auth.js
+++ apps/static/javascript/api/auth.js
@@ -6,6 +6,16 @@ const form = {
    usermail: document.getElementById("usermail"),
    password: document.getElementById("password"),
 };
+
+const reg_form = {
+   username: document.getElementById("usermail"),
+   email: document.getElementById("usermail"),
+   password: document.getElementById("password"),
+   retype_password: document.getElementById("password"),
+   workshop_name: document.getElementById("workshop_name"),
+   workshop_phone: document.getElementById("workshop_phone"),
+   workshop_address: document.getElementById("workshop_address"),
+};
 // **************************************************************
 // BASE INISIALIZATION | END
 // **************************************************************
@@ -67,6 +77,71 @@ function signin_process(e) {
       });
 }
 document.getElementById("signin_form").addEventListener("submit", signin_process);
+// **************************************************************
+// SIGN IN PROCESS | END
+// **************************************************************
+
+// **************************************************************
+// SIGN IN PROCESS | START
+// **************************************************************
+function signup_process(e) {
+   e.preventDefault();
+   const { usermail, password } = form;
+
+   // Set API Request Configuration
+   const API = "/auth/signin/account";
+   const my_headers = new Headers();
+   const raw = JSON.stringify({
+      usermail: usermail.value,
+      password: password.value,
+   });
+
+   my_headers.append("Content-Type", "application/json");
+   const request_options = {
+      method: "POST",
+      headers: my_headers,
+      body: raw,
+      redirect: "follow",
+   };
+
+   // Set Loading UI
+   swal.fire({
+      title: "Tunggu Sebentar..",
+      text: "Permintaan kamu sedang diproses",
+      button: false,
+   });
+
+   // Request API
+   fetch(API, request_options)
+      .then((http_response) => http_response.json())
+      .then((response) => {
+         if (response.status_code == 200) {
+            swal
+               .fire({
+                  title: "Login berhasil",
+                  icon: "success",
+               })
+               .then((result) => {
+                  window.location.replace("/dashboard/");
+               });
+         } else {
+            swal
+               .fire({
+                  title: `${response.description}`,
+                  icon: "error",
+                  buttons: "Kembali",
+               })
+               .then((result) => {
+                  window.location.replace("/auth/signin");
+               });
+         }
+      })
+      .catch((error) => {
+         swal.fire.close();
+         console.error(error);
+      });
+}
+document.getElementById("signup_form").addEventListener("submit", signup_process);
 // **************************************************************
 // SIGN IN PROCESS | END
 // **************************************************************
--- apps/static/javascript/helper/swal.js
+++ apps/static/javascript/helper/swal.js
@@ -2,66 +2,64 @@
 // SUCCESS ALERT | START
 // **************************************************************
 function swalSuccess(message) {
-      return Swal.fire({
-          icon: "success",
-          title: "Berhasil",
-          text: message,
-          timer: 1500,
-          showConfirmButton: false,
-      });
-  }
-  // **************************************************************
-  // SUCCESS ALERT | END
-  // **************************************************************
+   return Swal.fire({
+      icon: "success",
+      title: "Berhasil",
+      text: message,
+      timer: 1500,
+      showConfirmButton: false,
+   });
+}
+// **************************************************************
+// SUCCESS ALERT | END
+// **************************************************************
 
-  // **************************************************************
+// **************************************************************
 // ERROR ALERT | START
 // **************************************************************
 function swalError(message) {
-      return Swal.fire({
-          icon: "error",
-          title: "Gagal",
-          text: message,
-      });
-  }
-  // **************************************************************
-  // ERROR ALERT | END
-  // **************************************************************
+   return Swal.fire({
+      icon: "error",
+      title: "Gagal",
+      text: message,
+   });
+}
+// **************************************************************
+// ERROR ALERT | END
+// **************************************************************
 
-  // **************************************************************
+// **************************************************************
 // WARNING ALERT | START
 // **************************************************************
 function swalWarning(message) {
-      return Swal.fire({
-          icon: "warning",
-          title: "Peringatan",
-          text: message,
-      });
-  }
-  // **************************************************************
-  // WARNING ALERT | END
-  // **************************************************************
+   return Swal.fire({
+      icon: "warning",
+      title: "Peringatan",
+      text: message,
+   });
+}
+// **************************************************************
+// WARNING ALERT | END
+// **************************************************************
 
-  // **************************************************************
+// **************************************************************
 // DELETE ALERT | START
 // **************************************************************
 async function swalDelete() {
-
-      return await Swal.fire({
-          title: "Hapus Data?",
-          text: "Data yang dihapus tidak dapat dikembalikan.",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#d33",
-          cancelButtonColor: "#3085d6",
-          confirmButtonText: "Ya, Hapus",
-          cancelButtonText: "Batal",
-      });
-  
-  }
-  // **************************************************************
-  // DELETE ALERT | END
-  // **************************************************************
+   return await Swal.fire({
+      title: "Hapus Data?",
+      text: "Data yang dihapus tidak dapat dikembalikan.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Ya, Hapus",
+      cancelButtonText: "Batal",
+   });
+}
+// **************************************************************
+// DELETE ALERT | END
+// **************************************************************
 
 // **************************************************************
 // LOADING ALERT | START
